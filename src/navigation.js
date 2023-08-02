7cc264affcd45135c7bf8f413bbfccefbf0b0832
128343390+import-pandas-as-numpy@users.noreply.github.com <128343390+import-pandas-as-numpy@users.noreply.github.com>
2023-08-02 03:56:56 -0500

    Appeased linter.

--- src/navigation.js
+++ src/navigation.js
@@ -2,8 +2,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    { text: 'About',
-      href: getPermalink('/about')},
+    { text: 'About', href: getPermalink('/about') },
     {
       text: 'Research',
       href: getBlogPermalink(),
